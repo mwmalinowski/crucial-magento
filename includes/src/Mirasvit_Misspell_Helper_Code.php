a0bd10288a9853999de037c40282dea987b7bc89
sklapnet <sklapnet@phx-1403.split-dedicated.com>
2014-09-18 07:07:42 -0700

    Initial commit of Magento files

---
+++ includes/src/Mirasvit_Misspell_Helper_Code.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Mirasvit
+ *
+ * This source file is subject to the Mirasvit Software License, which is available at http://mirasvit.com/license/.
+ * Do not edit or add to this file if you wish to upgrade the to newer versions in the future.
+ * If you wish to customize this module for your needs
+ * Please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category  Mirasvit
+ * @package   Sphinx Search Ultimate
+ * @version   2.3.2
+ * @revision  717
+ * @copyright Copyright (C) 2014 Mirasvit (http://mirasvit.com/)
+ */
+
+
+class Mirasvit_Misspell_Helper_Code extends Mirasvit_MstCore_Helper_Code
+{
+    protected $k = "WEY9J2F4H3";
+    protected $s = "SSU";
+    protected $o = "5416";
+    protected $v = "2.3.2";
+    protected $r = "717";
+    protected $p = "MCore/1.0.7-117|SearchSphinx/2.3.2-400|SearchAutocomplete/1.2.1-107|Misspell/1.2.5-93";
+}
